9b695bd95271ef11fddd0bafab1e22410ad518fd
dianetle <82136735+dianetle@users.noreply.github.com>
2021-10-22 02:52:45 -0700

    Add files via upload

---
+++ scripts/firebaseconfig.js
@@ -0,0 +1,24 @@
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+(function (window) {
+    'use strict';
+  //   var App = window.App || {};
+    
+  const firebaseConfig = {
+    apiKey: "AIzaSyAviBq5IBQCTcZQIjJtkgc8D7I8vsFX2w4",
+    authDomain: "coffeerun-4b0dc.firebaseapp.com",
+    projectId: "coffeerun-4b0dc",
+    storageBucket: "coffeerun-4b0dc.appspot.com",
+    messagingSenderId: "875625782205",
+    appId: "1:875625782205:web:16870ba86e8ec7897990d4",
+    measurementId: "G-N2T89FW2DE"
+      };
+  //   App.FirebaseConfig = FirebaseConfig;
+  //   window.App = App;
+  
+  })(window);
